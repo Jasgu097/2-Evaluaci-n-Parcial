3dbee6a9adf011cf820a2222e1597f5cef883e02
Jasgu097 <124702997+Jasgu097@users.noreply.github.com>
2023-03-21 09:29:28 -0600

    Add files via upload

---
+++ Mayorque.py
@@ -0,0 +1,7 @@
+
+n1 = int(input("Introduzca el primer numero: "))
+n2 = int(input("Introduzca el segundo numero: "))
+if n1>n2:
+  print("El numero mas grande es: ", n1)
+else:
+  print("El numero mas grande es: ", n2)
